e7a02c5afd38211b58016eb4f515ac3ba6b5ce2b
Fiona Ho <fwhooo@hotmail.com>
2021-04-11 18:46:26 -0400

    edited join path syntax again to /

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ app.use(cors());
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     
-    // const path = require('path');
-    // app.get('*', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    // });
+    const path = require('path');
+    app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 // use an environment variable PORT in case of conflict 
