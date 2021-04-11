ae8d542bf7a809ff6c51ea9cf780ff389347989f
Fiona Ho <fwhooo@hotmail.com>
2021-04-11 18:27:44 -0400

    edited the join path in index.js again solid

--- index.js
+++ index.js
@@ -64,10 +64,12 @@ app.use(cors());
 
 // not sure if this is working....
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname+'/client/build')));
-    app.get('*', (req,res) => {
-      res.sendFile('index.html', {root:__dirname+'/client/build'})
-    })
+    app.use(express.static('client/build'));
+    
+    const path = require('path');
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 // use an environment variable PORT in case of conflict 
