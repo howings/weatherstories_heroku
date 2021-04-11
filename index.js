28e11ac1fc32b203b3dcfd8ae46b177a8be6e28e
Fiona Ho <fwhooo@hotmail.com>
2021-04-11 17:33:36 -0400

    edited the production control code

--- index.js
+++ index.js
@@ -58,8 +58,15 @@ app.use(cors());
 //     console.log(response.body);
 // });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+    app.get('*', (req,res) => {
+      res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+    })
 }
 
 // use an environment variable PORT in case of conflict 
