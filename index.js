1b464a25dd3d555b01c94d71b7115b6964d5b6f5
Fiona Ho <fwhooo@hotmail.com>
2021-04-11 17:54:16 -0400

    testing auto deploy

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ app.use(cors());
 //     app.use(express.static('client/build'));
 // }
 
+// not sure if this is working....
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', (req,res) => {
